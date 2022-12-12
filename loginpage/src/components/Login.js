732633f2e12d9b3488f46516990b0efd0159cc84
DEEPAK K <deepakkumar281020@gmail.com>
2022-12-12 11:01:59 +0530

    first commit

---
+++ loginpage/src/components/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Login.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+function Login () {
+    return(
+        <div>
+            <h1>Hiiiii</h1>
+            <label>Name</label>
+            <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="name" variant="outlined" />
+     
+    </Box>
+
+        </div>
+    )
+}
+export default Login;
